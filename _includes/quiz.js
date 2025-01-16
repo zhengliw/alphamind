302dc6372ee25ee6d5955799e37d75ee71183b11
Zhengli Wang <zhengliw@foxmail.com>
2025-01-16 10:35:26 +0100

    Fix handling for different question types

--- _includes/quiz.js
+++ _includes/quiz.js
@@ -2,38 +2,57 @@ function checkAnswers(quiz) {
 	let total = 0;
 	let correct = 0;
 	for(let i = 0; i < quiz.questions.length; i++) {
-		let subtotal = 0;
-		let subcorrect = 0;
-		let isRadio = false;
-		if(quiz.questions[i].type === "radio") {
-			subtotal = 1;
-			isRadio = true;
-		}
-		for(let j = 0; j < quiz.questions[i].options.length; j++) {
-			let option = document.getElementById(`question-${i + 1}-option-${j + 1}-input`);
-			let option_container = document.getElementById(`question-${i + 1}-option-${j + 1}`);
-			option_container.classList.remove("option-correct", "option-incorrect");
-			if(option.checked == quiz.questions[i].options[j].correct) {
-				option_container.classList.add("option-correct");
-				if(isRadio) {
-					subcorrect = 1;
-					break;
-				}
-			} else {
-				option_container.classList.add("option-incorrect");
-			}
-			if(!isRadio) {
-				subtotal++;
-			}
+		let subscore;
+		switch(quiz.questions[i].type) {
+			case "checkbox":
+				subscore = checkboxHandler(quiz.questions[i], i);
+				break;
+			case "radio":
+				subscore = radioHandler(quiz.questions[i], i);
+				break;
+			default:
+				subscore = {subtotal: 0, subcorrect: 0};
 		}
 		let score = document.getElementById(`question-${i + 1}-score`);
-		score.innerHTML = `${subcorrect} von ${subtotal} richtig`;
+		score.innerHTML = `${subscore.subcorrect} von ${subscore.subtotal} richtig`;
 		score.style.display = "block";
-		total += subtotal;
-		correct += subcorrect;
+		total += subscore.subtotal;
+		correct += subscore.subcorrect;
 	}
 	let total_score = document.getElementById(`total-score`);
 	total_score.innerText = `${correct} von ${total} richtig. Das entspricht ${(correct / total * 100).toFixed(0)}%.`;
+}
+
+function checkboxHandler(question, questionIndex) {
+	let subtotal = 0, subcorrect = 0;
+	for(let i = 0; i < question.options.length; i++) {
+		let option = document.getElementById(`question-${questionIndex + 1}-option-${i + 1}-input`);
+		let option_container = document.getElementById(`question-${questionIndex + 1}-option-${i + 1}`);
+		if(option.checked === question.options[i].correct) {
+			subcorrect++;
+			option_container.classList.add("option-correct");
+		} else {
+			option_container.classList.add("option-incorrect");
+		}
+		subtotal++;
+	}
+	return {subtotal: subtotal, subcorrect: subcorrect}
+}
+
+function radioHandler(question, questionIndex) {
+	let subtotal = 1, subcorrect = 0;
+	for(let i = 0; i < question.options.length; i++) {
+		let option = document.getElementById(`question-${questionIndex + 1}-option-${i + 1}-input`);
+		let option_container = document.getElementById(`question-${questionIndex + 1}-option-${i + 1}`);
+		if(option.checked === question.options[i].correct) {
+			subcorrect = 1;
+			option_container.classList.add("option-correct");
+			break;
+		} else {
+			option_container.classList.add("option-incorrect");
+		}
+	}
+	return {subtotal: subtotal, subcorrect: subcorrect}
 }
 
 addEventListener("submit", (e) => {
